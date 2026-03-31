03168521673cfea24f49694df72447b764962ffa
polacektomas <tomaspolacek@ibm.com>
2026-03-31 13:24:42 +0200

    NCL-9481 - Add exception handling for finders

--- core/src/main/java/org/jboss/pnc/deliverablesanalyzer/AnalyzerOrchestrator.java
+++ core/src/main/java/org/jboss/pnc/deliverablesanalyzer/AnalyzerOrchestrator.java
@@ -41,7 +41,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 
-// TODO Tomas: Global Exception Handling
 // TODO Tomas: Test Cancellation
 
 @ApplicationScoped
